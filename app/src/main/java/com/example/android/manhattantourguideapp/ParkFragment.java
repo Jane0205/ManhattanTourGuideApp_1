4b840f65766d9438ef0d9c6b334076d05cea98d2
Jane0205 <wosl0205@gmail.com>
2017-03-03 13:30:05 -0500

    v8

--- app/src/main/java/com/example/android/manhattantourguideapp/ParkFragment.java
+++ app/src/main/java/com/example/android/manhattantourguideapp/ParkFragment.java
@@ -28,11 +28,11 @@ public class ParkFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.detail_list,container,false);
 
         final ArrayList<Detail> details = new ArrayList<Detail>();
-        details.add(new Detail(R.string.park_name_1,R.string.park_location_1,R.drawable.park));
-        details.add(new Detail(R.string.park_name_2,R.string.park_location_2,R.drawable.park));
-        details.add(new Detail(R.string.park_name_3,R.string.park_location_3,R.drawable.park));
-        details.add(new Detail(R.string.park_name_4,R.string.park_location_4,R.drawable.park));
-        details.add(new Detail(R.string.park_name_5,R.string.park_location_5,R.drawable.park));
+        details.add(new Detail(R.string.park_name_1,R.string.park_location_1,R.drawable.park,R.string.park_num));
+        details.add(new Detail(R.string.park_name_2,R.string.park_location_2,R.drawable.park,R.string.park_num));
+        details.add(new Detail(R.string.park_name_3,R.string.park_location_3,R.drawable.park,R.string.park_num));
+        details.add(new Detail(R.string.park_name_4,R.string.park_location_4,R.drawable.park,R.string.park_num));
+        details.add(new Detail(R.string.park_name_5,R.string.park_location_5,R.drawable.park,R.string.park_num));
 
         DetailAdapter adapter = new DetailAdapter(getActivity(),details,R.color.park);
 
